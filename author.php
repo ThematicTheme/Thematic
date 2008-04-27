130007cd5d67d8704b7133c5e3b816c77a2b0702
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2008-04-27 01:02:52 +0000

    added antispambot() to author email
    git-svn-id: http://thematic.googlecode.com/svn/trunk@94 25245320-ac48-0410-b4b3-eb13123354aa

--- author.php
+++ author.php
@@ -22,7 +22,7 @@ foreach ($options as $value) {
                     <?php if ( !(''== $authordata->user_description) ) : echo apply_filters('archive_meta', $authordata->user_description); endif; ?>
                 </div>  			
     			<div id="author-email">
-                    <a class="email" title="<?php echo $authordata->user_email; ?>" href="mailto:<?php echo $authordata->user_email; ?>"><?php _e('Email ') ?><span class="fn n"><span class="given-name"><?php echo $authordata->first_name; ?></span> <span class="family-name"><?php echo $authordata->last_name; ?></span></span></a>
+                    <a class="email" title="<?php echo antispambot($authordata->user_email); ?>" href="mailto:<?php echo antispambot($authordata->user_email); ?>"><?php _e('Email ') ?><span class="fn n"><span class="given-name"><?php echo $authordata->first_name; ?></span> <span class="family-name"><?php echo $authordata->last_name; ?></span></span></a>
                     <a class="url"  style="display:none;" href="<?php echo get_option('home') ?>/"><?php bloginfo('name') ?></a>   
                 </div>
 			</div><!-- #author-info -->
