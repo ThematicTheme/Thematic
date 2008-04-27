7fc4ff52458ffad6f040918e8d8a57bca39f7874
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2008-04-27 18:29:11 +0000

    git-svn-id: http://thematic.googlecode.com/svn/trunk@96 25245320-ac48-0410-b4b3-eb13123354aa

--- archive.php
+++ archive.php
@@ -37,21 +37,23 @@ foreach ($options as $value) {
 
 			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
 				<h2 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf(__('Permalink to %s', 'thematic'), wp_specialchars(get_the_title(), 1)) ?>" rel="bookmark"><?php the_title() ?></a></h2>
-				<div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s &#8211; %2$s', 'thematic'), the_date('', '', '', false), get_the_time()) ?></abbr></div>
+				<div class="entry-meta">
+                    <?php /* if default setting of no author link */ if($thm_authorlink == 'false') { ?>
+                    					<span class="author vcard"><?php $author = get_the_author(); ?><?php _e('By ') ?><span class="fn n"><?php _e("$author") ?></span></span>
+                    <?php /* else show a link to the author page */ } else { ?>	
+                                        <span class="author vcard"><?php printf(__('By %s', 'thematic'), '<a class="url fn n" href="'.get_author_link(false, $authordata->ID, $authordata->user_nicename).'" title="' . sprintf(__('View all posts by %s', 'thematic'), $authordata->display_name) . '">'.get_the_author().'</a>') ?></span>
+                    <?php } ?>				    
+					<span class="meta-sep">|</span>
+    				<span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s', 'sandbox'), the_date('', '', '', false), get_the_time()) ?></abbr></span>
+				</div><!-- .entry-meta -->
 				<div class="entry-content">
 <?php the_excerpt(''.__('Read More <span class="meta-nav">&raquo;</span>', 'thematic').'') ?>
 
 				</div>
-				<div class="entry-meta">
-<?php /* if default setting of no author link */ if($thm_authorlink == 'false') { ?>
-					<span class="author vcard"><?php $author = get_the_author(); ?><?php _e('By ') ?><span class="fn n"><?php _e("$author") ?></span></span>
-<?php /* else show a link to the author page */ } else { ?>	
-                    <span class="author vcard"><?php printf(__('By %s', 'thematic'), '<a class="url fn n" href="'.get_author_link(false, $authordata->ID, $authordata->user_nicename).'" title="' . sprintf(__('View all posts by %s', 'thematic'), $authordata->display_name) . '">'.get_the_author().'</a>') ?></span>
-<?php } ?>				    
-					<span class="meta-sep">|</span>
-<?php edit_post_link(__('Edit', 'thematic'), "\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n"); ?>
+				<div class="entry-utility">
 					<span class="comments-link"><?php comments_popup_link(__('Leave a comment', 'thematic'), __('1 Comment', 'thematic'), __('% Comments', 'thematic')) ?></span>
-				</div>
+                    <?php edit_post_link(__('Edit', 'thematic'), "\t\t\t\t\t<span class=\"meta-sep\">|</span>\n<span class=\"edit-link\">", "</span>\t\t\t\t\t"); ?>
+				</div><!-- .entry-utility -->
 			</div><!-- .post -->
 
 <?php endwhile ?>
--- attachment.php
+++ attachment.php
@@ -15,38 +15,26 @@ foreach ($options as $value) {
 			<h1 class="page-title"><a href="<?php echo get_permalink($post->post_parent) ?>" rev="attachment"><?php echo get_the_title($post->post_parent) ?></a></h1>
 			<div id="post-<?php the_ID(); ?>" class="<?php sandbox_post_class() ?>">
 				<h2 class="entry-title"><?php the_title() ?></h2>
+				<div class="entry-meta">
+                    <?php /* if default setting of no author link */ if($thm_authorlink == 'false') { ?>
+                    					<span class="author vcard"><?php $author = get_the_author(); ?><?php _e('By ') ?><span class="fn n"><?php _e("$author") ?></span></span>
+                    <?php /* else show a link to the author page */ } else { ?>	
+                                        <span class="author vcard"><?php printf(__('By %s', 'thematic'), '<a class="url fn n" href="'.get_author_link(false, $authordata->ID, $authordata->user_nicename).'" title="' . sprintf(__('View all posts by %s', 'thematic'), $authordata->display_name) . '">'.get_the_author().'</a>') ?></span>
+                    <?php } ?>				    
+					<span class="meta-sep">|</span>
+    				<span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s', 'sandbox'), the_date('', '', '', false), get_the_time()) ?></abbr></span>
+				</div><!-- .entry-meta -->
 				<div class="entry-content">
 					<div class="entry-attachment"><?php the_attachment_link($post->post_ID, true) ?></div>
 <?php the_content(''.__('Read More <span class="meta-nav">&raquo;</span>', 'thematic').''); ?>
 
 					<?php wp_link_pages('before=<div class="page-link">' .__('Pages:', 'thematic') . '&after=</div>') ?>
 				</div>
-				<div class="entry-meta">
-<?php /* if default setting of no author link */ if($thm_authorlink == 'false') { ?>
-					<?php printf(__('This entry was written by %1$s on <abbr class="published" title="%2$sT%3$s">%4$s</abbr>. Bookmark the <a href="%8$s" title="Permalink to %9$s" rel="bookmark">permalink</a>. Follow any comments here with the <a href="%10$s" title="Comments RSS to %9$s" rel="alternate" type="application/rss+xml">RSS feed for this post</a>.', 'thematic'),
-						get_the_author(),
-						get_the_time('Y-m-d'),
-						get_the_time('H:i:sO'),
-						the_date('', '', '', false),
-						get_the_time(),
-						get_the_category_list(', '),
-						get_the_tag_list(' '.__('and tagged', 'thematic').' ', ', ', ''),
+				<div class="entry-utility">
+					<?php printf(__('Bookmark the <a href="%1$s" title="Permalink to %2$s" rel="bookmark">permalink</a>. Follow any comments here with the <a href="%3$s" title="Comments RSS to %2$s" rel="alternate" type="application/rss+xml">RSS feed for this post</a>.', 'thematic'),
 						get_permalink(),
 						wp_specialchars(get_the_title(), 'double'),
 						comments_rss() ) ?>
-<?php /* else show a link to the author page */ } else { ?>	
-					<?php printf(__('This entry was written by %1$s on <abbr class="published" title="%2$sT%3$s">%4$s</abbr>. Bookmark the <a href="%8$s" title="Permalink to %9$s" rel="bookmark">permalink</a>. Follow any comments here with the <a href="%10$s" title="Comments RSS to %9$s" rel="alternate" type="application/rss+xml">RSS feed for this post</a>.', 'thematic'),
-						'<span class="author vcard"><a class="url fn n" href="'.get_author_link(false, $authordata->ID, $authordata->user_nicename).'" title="' . sprintf(__('View all posts by %s', 'thematic'), $authordata->display_name) . '">'.get_the_author().'</a></span>',
-						get_the_time('Y-m-d'),
-						get_the_time('H:i:sO'),
-						the_date('', '', '', false),
-						get_the_time(),
-						get_the_category_list(', '),
-						get_the_tag_list(' '.__('and tagged', 'thematic').' ', ', ', ''),
-						get_permalink(),
-						wp_specialchars(get_the_title(), 'double'),
-						comments_rss() ) ?>
-<?php } ?>		    
 
 <?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) : // Comments and trackbacks open ?>
 					<?php printf(__('<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'thematic'), get_trackback_url()) ?>
@@ -59,7 +47,7 @@ foreach ($options as $value) {
 <?php endif; ?>
 <?php edit_post_link(__('Edit', 'thematic'), "\n\t\t\t\t\t<span class=\"edit-link\">", "</span>"); ?>
 
-				</div>
+				</div><!-- .entry-utility -->
 			</div><!-- .post -->
 
 <?php comments_template(); ?>
