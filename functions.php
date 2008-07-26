bd03affa4f58e8e91a1b5f1f96c89757c04b696d
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2008-07-26 04:22:47 +0000

    Filtered the post headers on index with thematic_postheader() — Going to add an if statement to take care of single.php
    git-svn-id: http://thematic.googlecode.com/svn/trunk@137 25245320-ac48-0410-b4b3-eb13123354aa

--- functions.php
+++ functions.php
@@ -15,8 +15,8 @@ require_once(THEMELIB . '/functions/page-numbers.php');
 // Load widgets
 require_once(THEMELIB . '/functions/widgets.php');
 
-// Load custom theme hooks
-require_once(THEMELIB . '/functions/custom-hooks.php');
+// Load custom theme hooks and filters
+require_once(THEMELIB . '/functions/hooks-filters.php');
 
 // Add Dynamic Contextual Semantic Classes
 require_once(THEMELIB . '/functions/sandbox-functions.php');
@@ -29,6 +29,6 @@ load_theme_textdomain('thematic');
 
 // Remove the WordPress Generator – via http://blog.ftwr.co.uk/archives/2007/10/06/improving-the-wordpress-generator/
 function thematic_remove_generators() { return ''; }  
-add_filter('the_generator','thematic_remove_generators'); 
+add_filter('the_generator','thematic_remove_generators');
 
 ?>
