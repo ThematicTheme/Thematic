81f96963409646738741ff69304bab826488f3f1
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2008-07-26 19:31:57 +0000

    Filtering the post footer with thematic_postfooter()
    git-svn-id: http://thematic.googlecode.com/svn/trunk@143 25245320-ac48-0410-b4b3-eb13123354aa

--- library/functions/hooks-filters.php
+++ library/functions/hooks-filters.php
@@ -18,8 +18,7 @@ function sandbox_globalnav() {
 
 // Information in Post Header
 function thematic_postheader() {
-    global $post;
-    global $authordata;
+    global $post, $authordata;
     
     if (is_single() || is_page()) {
         $posttitle = '<h1 class="entry-title">' . get_the_title() . "</h1>\n";
@@ -55,6 +54,89 @@ function thematic_postheader() {
     }
     
     echo apply_filters( 'thematic_postheader', $postheader ); // Filter to override default post header
+}
+
+// Information in Post Footer
+function thematic_postfooter() {
+    global $id, $post;
+    
+    // Display the post categories  
+    $postcategory = '<div class="entry-utility">';
+    $postcategory .= '<span class="cat-links">';
+    if (is_single()) {
+        $postcategory .= __('This entry was posted in ', 'thematic') . get_the_category_list(', ');
+        $postcategory .= '</span>';
+    } else {
+        $postcategory .= __('Posted in ', 'thematic') . get_the_category_list(', ');
+        $postcategory .= '</span> <span class="meta-sep">|</span>';
+    }
+    
+    // Display the tags
+    if (is_single()) {
+        $tagtext = __(' and tagged', 'thematic');
+        $posttags = get_the_tag_list("<span class=\"tag-links\"> $tagtext ",', ','</span>');
+    } else {
+        $tagtext = __('Tagged', 'thematic');
+        $posttags = get_the_tag_list("<span class=\"tag-links\"> $tagtext ",', ','</span> <span class="meta-sep">|</span>');
+    }
+    
+    // Display comments link and edit link
+    if (comments_open()) {
+        $postcommentnumber = get_comments_number();
+        if ($postcommentnumber > '1') {
+            $postcomments = ' <a href="' . get_permalink() . '#comments" title="' . __('Comment on ', 'thematic') . wp_specialchars(get_the_title(), 1) . '">';
+            $postcomments .= get_comments_number() . __(' Comments', 'thematic') . '</a>';
+        } elseif ($postcommentnumber == '1') {
+            $postcomments = ' <a href="' . get_permalink() . '#comments" title="' . __('Comment on ', 'thematic') . wp_specialchars(get_the_title(), 1) . '">';
+            $postcomments .= get_comments_number() . __(' Comment', 'thematic') . '</a>';
+        } elseif ($postcommentnumber == '0') {
+            $postcomments = ' <a href="' . get_permalink() . '#comments" title="' . __('Comment on ', 'thematic') . wp_specialchars(get_the_title(), 1) . '">';
+            $postcomments .= __('Leave a comment', 'thematic') . '</a>';
+        }
+    } else {
+        $postcomments = ' <span class="comments-link">Comments Closed</span>';
+    }
+    // Display edit link
+    if (current_user_can('edit_posts')) {
+        $postcomments .= '<span class="meta-sep"> |</span> <a href="' . get_bloginfo('wpurl') . '/wp-admin/post.php?action=edit&post=' . $id;
+        $postcomments .= '" title="' . __('Edit post', 'thematic') .'">';
+        $postcomments .= __('Edit', 'thematic') . '</a>';
+    }               
+    $postcomments .= "</div><!-- .entry-utility -->\n";
+    
+    // Display permalink, comments link, and RSS on single posts
+    $postconnect .= __('. Bookmark the ', 'thematic') . '<a href="' . get_permalink() . '" title="' . __('Permalink to ', 'thematic') . wp_specialchars(get_the_title(), 1) . '">';
+    $postconnect .= __('permalink', 'thematic') . '</a>.';
+    if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) { /* Comments are open */
+        $postconnect .= ' <a class="comment-link" href="#respond" title ="' . __('Post a comment', 'thematic') . '">' . __('Post a comment', 'thematic') . '</a>';
+        $postconnect .= __(' or leave a trackback: ', 'thematic');
+        $postconnect .= '<a class="trackback-link" href="' . trackback_url(FALSE) . '" title ="' . __('Trackback URL for your post', 'thematic') . '" rel="trackback">' . __('Trackback URL', 'thematic') . '</a>.';
+    } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) { /* Only trackbacks are open */
+        $postconnect .= __(' Comments are closed, but you can leave a trackback: ', 'thematic');
+        $postconnect .= '<a class="trackback-link" href="' . trackback_url(FALSE) . '" title ="' . __('Trackback URL for your post', 'thematic') . '" rel="trackback">' . __('Trackback URL', 'thematic') . '</a>.';
+    } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) { /* Only comments open */
+        $postconnect .= __(' Trackbacks are closed, but you can ', 'thematic');
+        $postconnect .= '<a class="comment-link" href="#respond" title ="' . __('Post a comment', 'thematic') . '">' . __('post a comment', 'thematic') . '</a>.';
+    } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) { /* Comments and trackbacks closed */
+        $postconnect .= __(' Both comments and trackbacks are currently closed.', 'thematic');
+    }
+    // Display edit link on single posts
+    if (current_user_can('edit_posts')) {
+        $postconnect .= ' <a href="' . get_bloginfo('wpurl') . '/wp-admin/post.php?action=edit&post=' . $id;
+        $postconnect .= '" title="' . __('Edit post', 'thematic') .'">';
+        $postconnect .= __('Edit', 'thematic') . '</a>';
+    }
+    $postconnect .= "</div><!-- .entry-utility -->\n";    
+    
+    // Add it all up
+    if (is_single()) {
+        $postfooter = $postcategory . $posttags . $postconnect;
+    } else {
+        $postfooter = $postcategory . $posttags . $postcomments;
+    }
+    
+    // Put it on the screen
+    echo apply_filters( 'thematic_postfooter', $postfooter ); // Filter to override default post footer
 }
 
 ?>
