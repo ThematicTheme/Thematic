3a99fc08d4939831b10fefe8022b34a626d2b432
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2008-07-26 20:52:08 +0000

    Filtering the post footer with thematic_postfooter()

--- attachment.php
+++ attachment.php
@@ -15,39 +15,13 @@ foreach ($options as $value) {
 			<h1 class="page-title"><a href="<?php echo get_permalink($post->post_parent) ?>" rev="attachment"><?php echo get_the_title($post->post_parent) ?></a></h1>
 			<div id="post-<?php the_ID(); ?>" class="<?php sandbox_post_class() ?>">
     			<?php thematic_postheader(); ?>
-				<div class="entry-meta">
-                    <?php /* if default setting of no author link */ if($thm_authorlink == 'false') { ?>
-                    					<span class="author vcard"><?php $author = get_the_author(); ?><?php _e('By ', 'thematic') ?><span class="fn n"><?php _e("$author") ?></span></span>
-                    <?php /* else show a link to the author page */ } else { ?>	
-                                        <span class="author vcard"><?php printf(__('By %s', 'thematic'), '<a class="url fn n" href="'.get_author_link(false, $authordata->ID, $authordata->user_nicename).'" title="' . sprintf(__('View all posts by %s', 'thematic'), $authordata->display_name) . '">'.get_the_author().'</a>') ?></span>
-                    <?php } ?>				    
-					<span class="meta-sep">|</span>
-    				<span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s', 'sandbox'), the_date('', '', '', false), get_the_time()) ?></abbr></span>
-				</div><!-- .entry-meta -->
 				<div class="entry-content">
 					<div class="entry-attachment"><?php the_attachment_link($post->post_ID, true) ?></div>
 <?php the_content(''.__('Read More <span class="meta-nav">&raquo;</span>', 'thematic').''); ?>
 
 					<?php wp_link_pages('before=<div class="page-link">' .__('Pages:', 'thematic') . '&after=</div>') ?>
 				</div>
-				<div class="entry-utility">
-					<?php printf(__('Bookmark the <a href="%1$s" title="Permalink to %2$s" rel="bookmark">permalink</a>. Follow any comments here with the <a href="%3$s" title="Comments RSS to %2$s" rel="alternate" type="application/rss+xml">RSS feed for this post</a>.', 'thematic'),
-						get_permalink(),
-						wp_specialchars(get_the_title(), 'double'),
-						comments_rss() ) ?>
-
-<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) : // Comments and trackbacks open ?>
-					<?php printf(__('<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'thematic'), get_trackback_url()) ?>
-<?php elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) : // Only trackbacks open ?>
-					<?php printf(__('Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'thematic'), get_trackback_url()) ?>
-<?php elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) : // Only comments open ?>
-					<?php printf(__('Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'thematic')) ?>
-<?php elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) : // Comments and trackbacks closed ?>
-					<?php _e('Both comments and trackbacks are currently closed.') ?>
-<?php endif; ?>
-<?php edit_post_link(__('Edit', 'thematic'), "\n\t\t\t\t\t<span class=\"edit-link\">", "</span>"); ?>
-
-				</div><!-- .entry-utility -->
+				<?php thematic_postfooter(); ?>
 			</div><!-- .post -->
 
 <?php comments_template(); ?>
