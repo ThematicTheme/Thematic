17432b084af0521040edff4129dbff17d91a6718
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2008-08-29 02:04:24 +0000

    Changed default avatar size to, um, default.
    git-svn-id: http://thematic.googlecode.com/svn/trunk@207 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/hooks-filters.php
+++ library/extensions/hooks-filters.php
@@ -207,7 +207,7 @@ function thematic_commenter_link() {
 		$commenter = ereg_replace( '(<a )/', '\\1class="url "' , $commenter );
 	}
 	$avatar_email = get_comment_author_email();
-	$avatar_size = apply_filters( 'avatar_size', '32' ); // Available filter: avatar_size
+	$avatar_size = apply_filters( 'avatar_size', '80' ); // Available filter: avatar_size
 	$avatar = str_replace( "class='avatar", "class='photo avatar", get_avatar( $avatar_email, $avatar_size ) );
 	echo $avatar . ' <span class="fn n">' . $commenter . '</span>';
 }
