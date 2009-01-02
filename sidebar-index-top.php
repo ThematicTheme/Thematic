4b209c89d05f2f2aaeccaac439f042ea2efcede0
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-01-02 13:27:38 +0000

    git-svn-id: http://thematic.googlecode.com/svn/trunk@245 25245320-ac48-0410-b4b3-eb13123354aa

--- sidebar-index-top.php
+++ sidebar-index-top.php
@@ -1,4 +1,4 @@
-<?php if ( function_exists('dynamic_sidebar') && is_sidebar_active(6) ) { // there is active widgets for this sidebar
+<?php if ( is_sidebar_active(6) ) { // there is active widgets for this sidebar
     echo '<div id="index-top" class="aside">'. "\n" . '<ul class="xoxo">' . "\n";
     dynamic_sidebar(6);
     echo '</ul>' . "\n" . '</div><!-- #index-top .aside -->'. "\n";
