3fc69bcc3cdab6b09e255e00e274eebc6ffce41b
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2009-01-24 23:38:52 +0000

    If post_excerpt exists it'll be used to create the meta-tag description otherwise thematic_excerpt_rss is used.

--- library/extensions/helpers.php
+++ library/extensions/helpers.php
@@ -5,12 +5,32 @@
 function thematic_the_excerpt($deprecated = '') {
 	global $post;
 	$output = '';
-	$output = $post->post_excerpt;
+	$output = strip_tags($post->post_excerpt);
 	if ( post_password_required($post) ) {
 		$output = __('There is no excerpt because this is a protected post.');
 		return $output;
 	}
 
 	return $output;
+	
 }
+
+// create bullet-proof excerpt for meta name="description"
+
+function thematic_excerpt_rss() {
+	global $post;
+	$output = '';
+	$output = strip_tags($post->post_excerpt);
+	if ( post_password_required($post) ) {
+		$output = __('There is no excerpt because this is a protected post.');
+		return $output;
+}
+
+	return apply_filters('thematic_excerpt_rss', $output);
+
+}
+
+add_filter('thematic_excerpt_rss', 'wp_trim_excerpt');
+
+
 ?>
