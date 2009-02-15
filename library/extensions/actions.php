5f28495f1d956f4f4fa1c143842fafe7645c6ad6
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-02-15 03:02:07 +0000

    Moved the dropdown function into actions.php and changed the name to follow Chris's style.

--- library/extensions/actions.php
+++ library/extensions/actions.php
@@ -76,4 +76,32 @@ function thematic_header_create_pingback() {
 function thematic_header_create_commentreply() {
   if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); // support for comment threading
 }
+
+// Load scripts for the jquery Superfish plugin
+// http://users.tpg.com.au/j_birch/plugins/superfish/#examples
+function thematic_header_superfish() { ?>
+
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/library/scripts/jquery-1.2.6.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/library/scripts/hoverIntent.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/library/scripts/superfish.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/library/scripts/supersubs.js"></script>
+
+<script type="text/javascript"> 
+ 
+    $(document).ready(function(){ 
+        $("ul.sf-menu").supersubs({ 
+            delay:       0,                                 // one second delay on mouseout 
+            animation:   {opacity:'show',height:'show'},    // fade-in and slide-down animation 
+            speed:       'fast',                            // faster animation speed            
+            minWidth:    12,                                // minimum width of sub-menus in em units 
+            maxWidth:    27,                                // maximum width of sub-menus in em units 
+            extraWidth:  1                                  // extra width can ensure lines don't sometimes turn over 
+                                                            // due to slight rounding differences and font-family 
+        }).superfish();
+    });
+ 
+</script>
+
+<?php }
+
 ?>
