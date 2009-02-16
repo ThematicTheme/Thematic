e9d5f51e774414a9828f08f538bc971a2f13e0c7
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2009-02-16 10:54:14 +0000

    Content of Title Tag can now be filtered
    git-svn-id: http://thematic.googlecode.com/svn/trunk@349 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/hooks-filters.php
+++ library/extensions/hooks-filters.php
@@ -60,6 +60,71 @@ function thematic_after() {
     do_action('thematic_after');
 }
 
+// Creates the content of the Title tag
+// Credits: Tarski Theme
+function thematic_doctitle() {
+
+    $site_name = get_bloginfo('name');
+    $separator = '|';
+        	
+    if ( is_single() ) {
+      $content = single_post_title('', FALSE);
+    }
+    elseif ( is_home() || is_front_page() ) { 
+      $content = get_bloginfo('description');
+    }
+    elseif ( is_page() ) { 
+      $content = single_post_title('', FALSE); 
+    }
+    elseif ( is_search() ) { 
+      $content = __('Search Results for:', 'thematic'); 
+      $content .= ' ' . wp_specialchars(stripslashes(get_search_query()), true);
+    }
+    elseif ( is_tag() ) { 
+      $content = __('Tag Archives:', 'thematic');
+      $content .= ' ' . thematic_tag_query();
+    }
+    elseif ( is_404() ) { 
+      $content = __('Not Found', 'thematic'); 
+    }
+    else { 
+      $content = get_bloginfo('description');
+    }
+
+    if (get_query_var('paged')) {
+      $content .= ' ' .$separator. ' ';
+      $content .= 'Page';
+      $content .= ' ';
+      $content .= get_query_var('paged');
+    }
+
+    if($content) {
+      $elements = array(
+        'site_name' => $site_name,
+        'separator' => $separator,
+        'content' => $content
+      );
+    } else {
+      $elements = array(
+        'site_name' => $site_name
+      );
+    }
+
+    // Filters should return an array
+    $elements = apply_filters('thematic_doctitle', $elements);
+	
+    // But if they don't, it won't try to implode
+    if(is_array($elements)) {
+      $doctitle = implode(' ', $elements);
+    }
+    else {
+      $doctitle = $elements;
+    }
+      
+    echo $doctitle;
+
+}
+
 // Add ID and CLASS attributes to the first <ul> occurence in wp_page_menu
 function thematic_add_menuclass($ulclass) {
 return preg_replace('/<ul>/', '<ul id="nav" class="sf-menu">', $ulclass, 1);
