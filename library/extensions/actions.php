f98faa6816fb012424693f846592b31c185564be
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-02-16 18:14:18 +0000

    Fixed the animation in the dropdowns.
    git-svn-id: http://thematic.googlecode.com/svn/trunk@351 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/actions.php
+++ library/extensions/actions.php
@@ -80,19 +80,20 @@ function thematic_head_superfish() { ?>
     
     <script type="text/javascript"> 
      
-        $(document).ready(function(){ 
-            $("ul.sf-menu").supersubs({ 
-                animation:   {opacity:'show',height:'show'},    // fade-in and slide-down animation 
-                speed:       'fast',                            // faster animation speed   
-                autoArrows:  false,                           // disable generation of arrow mark-up 
-                dropShadows: false,                            // disable drop shadows 
-                         
-                minWidth:    12,                                // minimum width of sub-menus in em units 
-                maxWidth:    27,                                // maximum width of sub-menus in em units 
-                extraWidth:  1                                  // extra width can ensure lines don't sometimes turn over 
-                                                                // due to slight rounding differences and font-family 
-            }).superfish();
-        });
+    $(document).ready(function(){ 
+        $("ul.sf-menu").supersubs({ 
+            minWidth:    12,   // minimum width of sub-menus in em units 
+            maxWidth:    27,   // maximum width of sub-menus in em units 
+            extraWidth:  1     // extra width can ensure lines don't sometimes turn over 
+                               // due to slight rounding differences and font-family 
+        }).superfish({ 
+            delay:       0,                            // one second delay on mouseout 
+            animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation 
+            speed:       'fast',                          // faster animation speed 
+            autoArrows:  false,                           // disable generation of arrow mark-up 
+            dropShadows: false                            // disable drop shadows 
+        }); 
+    }); 
      
     </script>
 
