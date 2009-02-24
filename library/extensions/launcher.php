fdbf9b8512d24afbe0a0537f365b89ef8b031251
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2009-02-24 16:13:31 +0000

    Removed the thematic_head action hook and included filters instead.

--- library/extensions/launcher.php
+++ library/extensions/launcher.php
@@ -1,12 +1,3 @@
 <?php
-add_action('thematic_head', 'thematic_head_create_head', 1);
-add_action('thematic_head', 'thematic_head_create_title', 2);
-add_action('thematic_head', 'thematic_head_create_contenttype', 3);
-add_action('thematic_head', 'thematic_head_create_description', 4);
-add_action('thematic_head', 'thematic_head_create_robots', 5);
-add_action('thematic_head', 'thematic_head_create_stylesheet', 6);
-add_action('thematic_head', 'thematic_head_create_rss', 7);
-add_action('thematic_head', 'thematic_head_create_pingback', 8);
-add_action('thematic_head', 'thematic_head_create_commentreply', 9);
 add_action('wp_head','thematic_head_scripts');
 ?>
