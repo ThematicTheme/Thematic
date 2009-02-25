4b425ab4d5c3192b0e73b501c4e8b1323c9a9961
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-02-25 03:47:56 +0000

    Everything in #header is now hooked in to thematic_header()

--- library/extensions/hooks-filters.php
+++ library/extensions/hooks-filters.php
@@ -12,6 +12,45 @@ function thematic_aboveheader() {
     do_action('thematic_aboveheader');
 }
 
+// Located in header.php
+// In the header div
+function thematic_header() {
+    do_action('thematic_header');
+}
+
+function thematic_brandingopen() { ?>
+    	<div id="branding">
+<?php }
+add_action('thematic_header','thematic_brandingopen',1);
+
+function thematic_blogtitle() { ?>
+    		<div id="blog-title"><span><a href="<?php echo get_option('home') ?>/" title="<?php bloginfo('name') ?>" rel="home"><?php bloginfo('name') ?></a></span></div>
+<?php }
+add_action('thematic_header','thematic_blogtitle',3);
+
+function thematic_blogdescription() {
+    		if (is_home() || is_front_page()) { ?>
+    		<h1 id="blog-description"><?php bloginfo('description') ?></h1>
+    		<?php } else { ?>	
+    		<div id="blog-description"><?php bloginfo('description') ?></div>
+    		<?php }
+}
+add_action('thematic_header','thematic_blogdescription',5);
+
+function thematic_brandingclose() { ?>
+    	</div><!--  #branding -->
+<?php }
+add_action('thematic_header','thematic_brandingclose',7);
+
+function thematic_access() { ?>
+    	<div id="access">
+    		<div class="skip-link"><a href="#content" title="<?php _e('Skip navigation to the content', 'thematic'); ?>"><?php _e('Skip to content', 'thematic'); ?></a></div>
+            <?php wp_page_menu('sort_column=menu_order') ?>
+        </div><!-- #access -->
+<?php }
+add_action('thematic_header','thematic_access',9);
+
+
 // Located in header.php 
 // Just after the header div
 function thematic_belowheader() {
