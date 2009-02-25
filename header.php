46e136661c195d257659169e5ff356e4f0eff001
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-02-25 03:48:05 +0000

    Everything in #header is now hooked in to thematic_header()

--- header.php
+++ header.php
@@ -25,18 +25,7 @@ wp_head(); ?>
 <?php thematic_aboveheader(); ?>   
 
     <div id="header">
-    	<div id="branding">
-    		<div id="blog-title"><span><a href="<?php echo get_option('home') ?>/" title="<?php bloginfo('name') ?>" rel="home"><?php bloginfo('name') ?></a></span></div>
-    		<?php if (is_home() || is_front_page()) { ?>
-    		<h1 id="blog-description"><?php bloginfo('description') ?></h1>
-    		<?php } else { ?>	
-    		<div id="blog-description"><?php bloginfo('description') ?></div>
-    		<?php } ?>
-    	</div><!--  #branding -->
-    	<div id="access">
-    		<div class="skip-link"><a href="#content" title="<?php _e('Skip navigation to the content', 'thematic'); ?>"><?php _e('Skip to content', 'thematic'); ?></a></div>
-            <?php wp_page_menu('sort_column=menu_order') ?>
-        </div><!-- #access -->
+        <?php thematic_header() ?>
     </div><!-- #header-->
     
 <?php thematic_belowheader(); ?>   
