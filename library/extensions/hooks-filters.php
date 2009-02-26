169b64ed691bcb9226cfec06b904e056eb85fb31
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-02-26 03:23:13 +0000

    added thematic_canonical_url();
    git-svn-id: http://thematic.googlecode.com/svn/trunk@389 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/hooks-filters.php
+++ library/extensions/hooks-filters.php
@@ -18,16 +18,19 @@ function thematic_header() {
     do_action('thematic_header');
 }
 
+// Open #branding
 function thematic_brandingopen() { ?>
     	<div id="branding">
 <?php }
 add_action('thematic_header','thematic_brandingopen',1);
 
+// Create the blog title
 function thematic_blogtitle() { ?>
     		<div id="blog-title"><span><a href="<?php echo get_option('home') ?>/" title="<?php bloginfo('name') ?>" rel="home"><?php bloginfo('name') ?></a></span></div>
 <?php }
 add_action('thematic_header','thematic_blogtitle',3);
 
+// Create the blog description
 function thematic_blogdescription() {
     		if (is_home() || is_front_page()) { ?>
     		<h1 id="blog-description"><?php bloginfo('description') ?></h1>
@@ -37,11 +40,13 @@ function thematic_blogdescription() {
 }
 add_action('thematic_header','thematic_blogdescription',5);
 
+// Close #branding
 function thematic_brandingclose() { ?>
     	</div><!--  #branding -->
 <?php }
 add_action('thematic_header','thematic_brandingclose',7);
 
+// Create #access
 function thematic_access() { ?>
     	<div id="access">
     		<div class="skip-link"><a href="#content" title="<?php _e('Skip navigation to the content', 'thematic'); ?>"><?php _e('Skip to content', 'thematic'); ?></a></div>
@@ -181,6 +186,17 @@ function thematic_create_contenttype() {
     $content .= "/>";
     $content .= "\n\n";
     echo apply_filters('thematic_create_contenttype', $content);
+}
+
+// Creates the canonical URL
+function thematic_canonical_url() {
+    if ( is_singular() ) {
+        $canonical_url ="\t";
+        $canonical_url .= '<link rel="canonical" href="' . get_permalink() . '"/>';
+        $canonical_url .= "\n\n";        
+        echo apply_filters('thematic_canonical_url', $canonical_url);
+    }
+
 }
 
 // Creates the meta-tag description
