4154013b5a85ec26ec93ec84dc834b959cfb0f1c
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-04-18 21:52:39 +0000

    Collapse the main asides when they're empty. Stop checking to see if dynamic_sidebar() exists (of course!).

--- sidebar.php
+++ sidebar.php
@@ -1,23 +1,21 @@
 <?php thematic_abovemainasides(); ?>
 
+<?php if (is_sidebar_active('primary-aside')) { ?>
 	<div id="primary" class="aside main-aside">
 		<ul class="xoxo">
-		
-<?php if (!dynamic_sidebar('primary-aside') ) : // begin primary sidebar widgets ?>
-<?php endif; // end primary sidebar widgets  ?>
-
+	<?php dynamic_sidebar('primary-aside'); ?>
 		</ul>
 	</div><!-- #primary .aside -->
+<?php } ?>		
 
 <?php thematic_betweenmainasides(); ?>
 
+<?php if (is_sidebar_active('secondary-aside')) { ?>
 	<div id="secondary" class="aside main-aside">
 		<ul class="xoxo">
-
-<?php if (!dynamic_sidebar('secondary-aside') ) : // begin  secondary sidebar widgets ?>
-<?php endif; // end secondary sidebar widgets  ?>
-
+<?php dynamic_sidebar('secondary-aside') ?>
 		</ul>
 	</div><!-- #secondary .aside -->
+<?php } ?>		
 	
 <?php thematic_belowmainasides(); ?>
