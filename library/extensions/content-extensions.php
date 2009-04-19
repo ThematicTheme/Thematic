178ab323427832304ea08eb4eed0c7924a2601eb
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2009-04-19 21:26:27 +0000

    added functions: thematic_next_post_link() thematic_previous_post_link()
    added filters:
    thematic_next_post_link_args 
    thematic_previous_post_link_args
    
    both need an array to be returned
    
    These filters are essential for a CMS / Portfolio site
    
    
    git-svn-id: http://thematic.googlecode.com/svn/trunk@475 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/content-extensions.php
+++ library/extensions/content-extensions.php
@@ -6,4 +6,22 @@ function more_text() {
 	return apply_filters('more_text', $content);
 }
 
+function thematic_previous_post_link() {
+	$args = array ('format'              => '%link',
+								 'link'                => '<span class="meta-nav">&laquo;</span> %title',
+								 'in_same_cat'         => FALSE,
+								 'excluded_categories' => '');
+	$args = apply_filters('thematic_previous_post_link_args', $args );
+	previous_post_link($args['format'], $args['link'], $args['in_same_cat'], $args['excluded_categories']);
+}
+
+function thematic_next_post_link() {
+	$args = array ('format'              => '%link',
+								 'link'                => '%title <span class="meta-nav">&raquo;</span>',
+								 'in_same_cat'         => FALSE,
+								 'excluded_categories' => '');
+	$args = apply_filters('thematic_next_post_link_args', $args );
+	next_post_link($args['format'], $args['link'], $args['in_same_cat'], $args['excluded_categories']);
+}
+
 ?>
