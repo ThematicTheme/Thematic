068ff9ce930cd90eb26c54c6beb0c6662a3bf990
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-04-25 16:31:29 +0000

    moved author-info-avatar into content-extensions

--- library/extensions/content-extensions.php
+++ library/extensions/content-extensions.php
@@ -4,7 +4,7 @@
 function more_text() {
 	$content = ''.__('Read More <span class="meta-nav">&raquo;</span>', 'thematic').'';
 	return apply_filters('more_text', $content);
-}
+} // end more_text
 
 function thematic_previous_post_link() {
 	$args = array ('format'              => '%link',
@@ -13,7 +13,7 @@ function thematic_previous_post_link() {
 								 'excluded_categories' => '');
 	$args = apply_filters('thematic_previous_post_link_args', $args );
 	previous_post_link($args['format'], $args['link'], $args['in_same_cat'], $args['excluded_categories']);
-}
+} // end thematic_previous_post_link
 
 function thematic_next_post_link() {
 	$args = array ('format'              => '%link',
@@ -22,6 +22,14 @@ function thematic_next_post_link() {
 								 'excluded_categories' => '');
 	$args = apply_filters('thematic_next_post_link_args', $args );
 	next_post_link($args['format'], $args['link'], $args['in_same_cat'], $args['excluded_categories']);
-}
+} // end thematic_next_post_link
+
+// Produces an avatar image with the hCard-compliant photo class for author info
+function thematic_author_info_avatar() {
+    global $wp_query; $curauth = $wp_query->get_queried_object();
+	$email = $curauth->user_email;
+	$avatar = str_replace( "class='avatar", "class='photo avatar", get_avatar("$email") );
+	echo $avatar;
+} // end thematic_author_info_avatar
 
 ?>
