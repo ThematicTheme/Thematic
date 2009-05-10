335ddcb3b7ad7b0b50174ae58e0b2e258b77e345
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-10 11:08:32 +0000

    Created the action hooks thematic_navigation_above() and thematic_navigation_below() .. the actions thematic_nav_above() and thematic_nav_below() are connected to the action hooks using priority 2.
    Navigation can now be removed using conditional tags or it can be exchanged against something completely different.
    
    git-svn-id: http://thematic.googlecode.com/svn/trunk@500 25245320-ac48-0410-b4b3-eb13123354aa

--- search.php
+++ search.php
@@ -14,14 +14,7 @@ foreach ($options as $value) {
 
 		<h1 class="page-title"><?php _e('Search Results for:', 'thematic') ?> <span id="search-terms"><?php echo wp_specialchars(stripslashes($_GET['s']), true); ?></span></h1>
 
-			<div id="nav-above" class="navigation">
-                <?php if(function_exists('wp_pagenavi')) { ?>
-                <?php wp_pagenavi(); ?>
-                <?php } else { ?>  
-				<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts', 'thematic')) ?></div>
-				<div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&raquo;</span>', 'thematic')) ?></div>
-				<?php } ?>
-			</div>
+			<?php thematic_navigation_above();?>
 			
 <?php thematic_above_searchloop() ?>			
 
@@ -40,14 +33,7 @@ foreach ($options as $value) {
 
 <?php thematic_below_searchloop() ?>			
 
-			<div id="nav-below" class="navigation">
-                <?php if(function_exists('wp_pagenavi')) { ?>
-                <?php wp_pagenavi(); ?>
-                <?php } else { ?>  
-				<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts', 'thematic')) ?></div>
-				<div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&raquo;</span>', 'thematic')) ?></div>
-				<?php } ?>
-			</div>
+			<?php thematic_navigation_below();?>
 
 <?php else : ?>
 
