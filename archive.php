cb800eae670e6157da16fa4f4fa994a6317f6f52
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-10 11:35:25 +0000

    Missed archive.php for my last update.
    git-svn-id: http://thematic.googlecode.com/svn/trunk@501 25245320-ac48-0410-b4b3-eb13123354aa

--- archive.php
+++ archive.php
@@ -24,14 +24,7 @@ foreach ($options as $value) {
 
 <?php rewind_posts() ?>
 
-			<div id="nav-above" class="navigation">
-                <?php if(function_exists('wp_pagenavi')) { ?>
-                <?php wp_pagenavi(); ?>
-                <?php } else { ?>  
-				<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts', 'thematic')) ?></div>
-				<div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&raquo;</span>', 'thematic')) ?></div>
-				<?php } ?>
-			</div>
+			<?php thematic_navigation_above();?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 
@@ -46,14 +39,7 @@ foreach ($options as $value) {
 
 <?php endwhile ?>
 
-			<div id="nav-below" class="navigation">
-                <?php if(function_exists('wp_pagenavi')) { ?>
-                <?php wp_pagenavi(); ?>
-                <?php } else { ?>  
-				<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts', 'thematic')) ?></div>
-				<div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&raquo;</span>', 'thematic')) ?></div>
-				<?php } ?>
-			</div>
+			<?php thematic_navigation_below();?>
 
 		</div><!-- #content .hfeed -->
 	</div><!-- #container -->
