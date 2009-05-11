38bf281ad924342b208bc42860fa6b3e22ae2c5e
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-11 11:56:39 +0000

    Added action hooks above and below the index inserts.

--- sidebar-index-bottom.php
+++ sidebar-index-bottom.php
@@ -1,5 +1,9 @@
+<?php thematic_aboveindexbottom() ?>
+
 <?php if ( function_exists('dynamic_sidebar') && is_sidebar_active('index-bottom') ) { // there is active widgets for this sidebar
     echo '<div id="index-bottom" class="aside">'. "\n" . '<ul class="xoxo">' . "\n";
     dynamic_sidebar('index-bottom');
     echo '</ul>' . "\n" . '</div><!-- #index-bottom .aside -->'. "\n";
 } ?>
+
+<?php thematic_belowindexbottom() ?>
--- sidebar-index-top.php
+++ sidebar-index-top.php
@@ -1,5 +1,9 @@
+<?php thematic_aboveindextop() ?>
+
 <?php if ( is_sidebar_active('index-top') ) { // there is active widgets for this sidebar
     echo '<div id="index-top" class="aside">'. "\n" . '<ul class="xoxo">' . "\n";
     dynamic_sidebar('index-top');
     echo '</ul>' . "\n" . '</div><!-- #index-top .aside -->'. "\n";
 } ?>
+
+<?php thematic_belowindextop() ?>
