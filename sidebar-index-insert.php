8fe4e85c8d7fa0939c024f32fd42a8ef770aba84
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-11 11:56:39 +0000

    Added action hooks above and below the index inserts.
    git-svn-id: http://thematic.googlecode.com/svn/trunk@507 25245320-ac48-0410-b4b3-eb13123354aa

--- sidebar-index-insert.php
+++ sidebar-index-insert.php
@@ -1,5 +1,10 @@
+<?php thematic_aboveindexinsert() ?>
+
 <?php if ( function_exists('dynamic_sidebar') && is_sidebar_active('index-insert') ) { // there is active widgets for this sidebar
     echo '<div id="index-insert" class="aside">'. "\n" . '<ul class="xoxo">' . "\n";
     dynamic_sidebar('index-insert');
     echo '</ul>' . "\n" . '</div><!-- #index-insert .aside -->'. "\n";
 } ?>
+
+
+<?php thematic_belowindexinsert() ?>
