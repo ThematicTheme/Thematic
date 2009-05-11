9cccdc6413494a5dbaa40e8c5c3075d829280615
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-11 11:56:27 +0000

    Added action hooks above and below the index inserts.

--- library/extensions/sidebar-extensions.php
+++ library/extensions/sidebar-extensions.php
@@ -1,5 +1,6 @@
 <?php
 
+
 // Filter to create the sidebar
 function thematic_sidebar() {
 
@@ -12,25 +13,75 @@ function thematic_sidebar() {
     get_sidebar();}
 	
 	return;
-}
+} // end thematic_sidebar
 
 
-// Located in sidebar.php 
-// Just before the main asides (commonly used as sidebars)
-function thematic_abovemainasides() {
-    do_action('thematic_abovemainasides');
-} // end thematic_abovemainasides
+// Main Aside Hooks
 
 
-// Located in sidebar.php 
-// Between the main asides (commonly used as sidebars)
-function thematic_betweenmainasides() {
-    do_action('thematic_betweenmainasides');
-} // end thematic_betweenmainasides
+	// Located in sidebar.php 
+	// Just before the main asides (commonly used as sidebars)
+	function thematic_abovemainasides() {
+	    do_action('thematic_abovemainasides');
+	} // end thematic_abovemainasides
+	
+	
+	// Located in sidebar.php 
+	// Between the main asides (commonly used as sidebars)
+	function thematic_betweenmainasides() {
+	    do_action('thematic_betweenmainasides');
+	} // end thematic_betweenmainasides
+	
+	
+	// Located in sidebar.php 
+	// after the main asides (commonly used as sidebars)
+	function thematic_belowmainasides() {
+	    do_action('thematic_belowmainasides');
+	} // end thematic_belowmainasides
+	
+
+// Index Aside Hooks
+
+	
+	// Located in sidebar-index-top.php
+	function thematic_aboveindextop() {
+		do_action('thematic_aboveindextop');
+	} // end thematic_aboveindextop
+	
+	
+	// Located in sidebar-index-top.php
+	function thematic_belowindextop() {
+		do_action('thematic_belowindextop');
+	} // end thematic_belowindextop
+	
+	
+	// Located in sidebar-index-insert.php
+	function thematic_aboveindexinsert() {
+		do_action('thematic_aboveindexinsert');
+	} // end thematic_aboveindexinsert
+	
+	
+	// Located in sidebar-index-insert.php
+	function thematic_belowindexinsert() {
+		do_action('thematic_belowindexinsert');
+	} // end thematic_belowindexinsert	
+	
+
+	// Located in sidebar-index-bottom.php
+	function thematic_aboveindexbottom() {
+		do_action('thematic_aboveindexbottom');
+	} // end thematic_aboveindexbottom
+	
+	
+	// Located in sidebar-index-bottom.php
+	function thematic_belowindexbottom() {
+		do_action('thematic_belowindexbottom');
+	} // end thematic_belowindexbottom	
+	
 
 
-// Located in sidebar.php 
-// after the main asides (commonly used as sidebars)
-function thematic_belowmainasides() {
-    do_action('thematic_belowmainasides');
-} // end thematic_belowmainasides
+// Page Aside Hooks
+
+
+// Subsidiary Aside Hooks
+
