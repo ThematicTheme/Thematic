c2e9db4d0750267361327f1bce60e7555f318287
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-11 12:07:56 +0000

    Added action hooks above and below the page asides

--- sidebar-page-bottom.php
+++ sidebar-page-bottom.php
@@ -1,5 +1,9 @@
+<?php thematic_abovepagebottom() ?>
+
 <?php if ( function_exists('dynamic_sidebar') && is_sidebar_active('page-bottom') ) { // there is active widgets for this sidebar
     echo '<div id="page-bottom" class="aside">'. "\n" . '<ul class="xoxo">' . "\n";
     dynamic_sidebar('page-bottom');
     echo '</ul>' . "\n" . '</div><!-- #page-bottom .aside -->'. "\n";
 } ?>
+
+<?php thematic_belowpagebottom() ?>
