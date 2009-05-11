0b4095b8f67cddc3d34a93f27cf7d60ccc41f5b6
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-11 12:05:23 +0000

    Added action hooks above and below the single post asides.
    git-svn-id: http://thematic.googlecode.com/svn/trunk@509 25245320-ac48-0410-b4b3-eb13123354aa

--- sidebar-single-bottom.php
+++ sidebar-single-bottom.php
@@ -1,5 +1,9 @@
+<?php thematic_abovesinglebottom() ?>
+
 <?php if ( function_exists('dynamic_sidebar') && is_sidebar_active('single-bottom') ) { // there is active widgets for this sidebar
     echo '<div id="single-bottom" class="aside">'. "\n" . '<ul class="xoxo">' . "\n";
     dynamic_sidebar('single-bottom');
     echo '</ul>' . "\n" . '</div><!-- #single-bottom .aside -->'. "\n";
 } ?>
+
+<?php thematic_belowsinglebottom() ?>
--- sidebar-single-top.php
+++ sidebar-single-top.php
@@ -1,5 +1,9 @@
+<?php thematic_abovesingletop() ?>
+
 <?php if ( function_exists('dynamic_sidebar') && is_sidebar_active('single-top') ) { // there is active widgets for this sidebar
     echo '<div id="single-top" class="aside">'. "\n" . '<ul class="xoxo">' . "\n";
     dynamic_sidebar('single-top');
     echo '</ul>' . "\n" . '</div><!-- #single-top .aside -->' . "\n";
 } ?>
+
+<?php thematic_belowsingletop() ?>
