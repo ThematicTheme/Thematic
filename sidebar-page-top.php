e079f10002735347ea263906985d08bb97c51184
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-11 12:07:56 +0000

    Added action hooks above and below the page asides
    git-svn-id: http://thematic.googlecode.com/svn/trunk@511 25245320-ac48-0410-b4b3-eb13123354aa

--- sidebar-page-top.php
+++ sidebar-page-top.php
@@ -1,5 +1,9 @@
+<?php thematic_abovepagetop() ?>
+
 <?php if ( function_exists('dynamic_sidebar') && is_sidebar_active('page-top') ) { // there is active widgets for this sidebar
     echo '<div id="page-top" class="aside">'. "\n" . '<ul class="xoxo">' . "\n";
     dynamic_sidebar('page-top');
     echo '</ul>' . "\n" . '</div><!-- #page-top .aside -->'. "\n";
 } ?>
+
+<?php thematic_belowpagetop() ?>
