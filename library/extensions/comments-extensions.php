ac8c45dc525d47f095b591835d8b9ef511a63cdb
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-13 12:02:58 +0000

    Comments are now added by hooking comments_template() into thematic_comments_template() via thematic_include_comments(). I gave include_comments a position of 5 so stuff could be injected fore and aft of it.
    git-svn-id: http://thematic.googlecode.com/svn/trunk@520 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/comments-extensions.php
+++ library/extensions/comments-extensions.php
@@ -1,10 +1,12 @@
 <?php
 
+
 // Located in comments.php
 // Just before #comments
 function thematic_abovecomments() {
     do_action('thematic_abovecomments');
 }
+
 
 // Located in comments.php
 // Just before #comments-list
@@ -12,11 +14,13 @@ function thematic_abovecommentslist() {
     do_action('thematic_abovecommentslist');
 }
 
+
 // Located in comments.php
 // Just after #comments-list
 function thematic_belowcommentslist() {
     do_action('thematic_belowcommentslist');
 }
+
 
 // Located in comments.php
 // Just before #trackbacks-list
@@ -24,11 +28,13 @@ function thematic_abovetrackbackslist() {
     do_action('thematic_abovetrackbackslist');
 }
 
+
 // Located in comments.php
 // Just after #trackbacks-list
 function thematic_belowtrackbackslist() {
     do_action('thematic_belowtrackbackslist');
 }
+
 
 // Located in comments.php
 // Just before the comments form
@@ -36,11 +42,13 @@ function thematic_abovecommentsform() {
     do_action('thematic_abovecommentsform');
 }
 
+
 // Adds the Subscribe to comments button
 function thematic_show_subscription_checkbox() {
     if(function_exists('show_subscription_checkbox')) { show_subscription_checkbox(); }
 }
 add_action('comment_form', 'thematic_show_subscription_checkbox', 98);
+
 
 // Located in comments.php
 // Just after the comments form
@@ -48,11 +56,13 @@ function thematic_belowcommentsform() {
     do_action('thematic_belowcommentsform');
 }
 
+
 // Adds the Subscribe without commenting button
 function thematic_show_manual_subscription_form() {
     if(function_exists('show_manual_subscription_form')) { show_manual_subscription_form(); }
 }
 add_action('thematic_belowcommentsform', 'thematic_show_manual_subscription_form', 5);
+
 
 // Located in comments.php
 // Just after #comments
@@ -60,11 +70,13 @@ function thematic_belowcomments() {
     do_action('thematic_belowcomments');
 }
 
+
 // creates the list comments arguments
 function list_comments_arg() {
 	$content = 'type=comment&callback=thematic_comments';
 	return apply_filters('list_comments_arg', $content);
 }
+
 
 // Produces an avatar image with the hCard-compliant photo class
 function thematic_commenter_link() {
@@ -80,4 +92,18 @@ function thematic_commenter_link() {
 	echo $avatar . ' <span class="fn n">' . $commenter . '</span>';
 } // end thematic_commenter_link
 
-?>
+
+// A hook for the standard comments template
+function thematic_comments_template() {
+	do_action('thematic_comments_template');
+} // end thematic_comments
+
+
+	// The standard comments template is injected into thematic_comments_template() by default
+	function thematic_include_comments() {
+		comments_template('', true);
+	} // end thematic_include_comments
+	
+	add_action('thematic_comments_template','thematic_include_comments',5);
+	
+	
