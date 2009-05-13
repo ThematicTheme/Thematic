306289c9c069f3b8fd2465459e0c64fab28d4e1c
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-13 03:08:08 +0000

    Added action hooks to the subsidiary asides.

--- sidebar-subsidiary.php
+++ sidebar-subsidiary.php
@@ -1,25 +1,37 @@
+<?php thematic_abovesubasides() ?>
+
 <?php if ( function_exists('dynamic_sidebar') ) { ?>
     <?php if ( is_sidebar_active('1st-subsidiary-aside') || is_sidebar_active('2nd-subsidiary-aside') || is_sidebar_active('3rd-subsidiary-aside') ) { // one of the subsidiary asides has a widget ?>
     <div id="subsidiary">
+    
+    		<?php thematic_before_first_sub() ?>
     
         <?php if ( function_exists('dynamic_sidebar') && is_sidebar_active('1st-subsidiary-aside') ) { // there are active widgets for this aside
             echo '<div id="first" class="aside sub-aside">'. "\n" . '<ul class="xoxo">' . "\n";
             dynamic_sidebar('1st-subsidiary-aside');
             echo '</ul>' . "\n" . '</div><!-- #first .aside -->'. "\n";
-        } ?>                
+        } ?>         
+        
+        <?php thematic_between_firstsecond_sub() ?>       
     
         <?php if ( function_exists('dynamic_sidebar') && is_sidebar_active('2nd-subsidiary-aside') ) { // there are active widgets for this aside
             echo '<div id="second" class="aside sub-aside">'. "\n" . '<ul class="xoxo">' . "\n";
             dynamic_sidebar('2nd-subsidiary-aside');
             echo '</ul>' . "\n" . '</div><!-- #second .aside -->'. "\n";
         } ?>       
+        
+        <?php thematic_between_secondthird_sub() ?>
    
         <?php if ( function_exists('dynamic_sidebar') && is_sidebar_active('3rd-subsidiary-aside') ) { // there are active widgets for this aside
             echo '<div id="third" class="aside sub-aside">'. "\n" . '<ul class="xoxo">' . "\n";
             dynamic_sidebar('3rd-subsidiary-aside');
             echo '</ul>' . "\n" . '</div><!-- #third .aside -->'. "\n";
-        } ?>        
+        } ?>       
+        
+        <?php thematic_after_third_sub() ?> 
         
     </div><!-- #subsidiary -->
     <?php } ?>
 <?php } ?>
+
+<?php thematic_belowsubasides() ?>
