a047c32fa7bfdac8a5613549bc457a9f0f120b9a
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-13 03:25:56 +0000

    Added thematic_404() and thematic_404_content(). So people could have some more fun with their 404 pages.
    git-svn-id: http://thematic.googlecode.com/svn/trunk@519 25245320-ac48-0410-b4b3-eb13123354aa

--- 404.php
+++ 404.php
@@ -5,16 +5,9 @@
 		<div id="content">
 
 			<div id="post-0" class="post error404">
-    			<?php thematic_postheader(); ?>
-				<div class="entry-content">
-					<p><?php _e('Apologies, but we were unable to find what you were looking for. Perhaps  searching will help.', 'thematic') ?></p>
-				</div>
-				<form id="error404-searchform" method="get" action="<?php bloginfo('home') ?>">
-					<div>
-						<input id="error404-s" name="s" type="text" value="<?php echo wp_specialchars(stripslashes($_GET['s']), true) ?>" size="40" />
-						<input id="error404-searchsubmit" name="searchsubmit" type="submit" value="<?php _e('Find', 'thematic') ?>" />
-					</div>
-				</form>
+			
+			<?php thematic_404() ?>
+			
 			</div><!-- .post -->
 
 		</div><!-- #content -->
