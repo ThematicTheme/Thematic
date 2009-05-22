4b4c52bdadbf73bddecae282741878668369b47e
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2009-05-22 13:54:30 +0000

    The loop in search.php is now an action (thematic_search_loop) hooked into thematic_searchloop()

--- search.php
+++ search.php
@@ -18,18 +18,7 @@ foreach ($options as $value) {
 			
 <?php thematic_above_searchloop() ?>			
 
-<?php while ( have_posts() ) : the_post(); ?>
-
-			<div id="post-<?php the_ID() ?>" class="<?php thematic_post_class() ?>">
-    			<?php thematic_postheader(); ?>
-				<div class="entry-content">
-<?php thematic_content(); ?>
-
-				</div>
-				<?php thematic_postfooter(); ?>
-			</div><!-- .post -->
-
-<?php endwhile; ?>
+<?php thematic_searchloop() ?>
 
 <?php thematic_below_searchloop() ?>			
 
