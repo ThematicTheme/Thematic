2b6fccef72ae7b885f93b20c42204c9ecbf56d97
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-06-20 02:45:48 +0000

    Fixed an error we inherited from The Sandbox http://www.plaintxt.org/2009/06/vulnerability-in-sandbox-fixed/
    git-svn-id: http://thematic.googlecode.com/svn/trunk@537 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/dynamic-classes.php
+++ library/extensions/dynamic-classes.php
@@ -176,6 +176,8 @@ function thematic_body_class( $print = true ) {
 
 	// Paged classes; for 'page X' classes of index, single, etc.
 	if ( ( ( $page = $wp_query->get('paged') ) || ( $page = $wp_query->get('page') ) ) && $page > 1 ) {
+	// Thanks to Prentiss Riddle, twitter.com/pzriddle, for the security fix below. 
+ 			$page = intval($page); // Ensures that an integer (not some dangerous script) is passed for the variable
 		$c[] = 'paged-' . $page;
 		if ( is_single() ) {
 			$c[] = 'single-paged-' . $page;
