9afa714f2cac7d3e64178ae5b677b859aafd35a6
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2009-08-17 19:19:35 +0000

    FIXED: changed thematic_author_loop to thematic_authorloop
    Thanks, Allan!

--- author.php
+++ author.php
@@ -25,12 +25,12 @@ foreach ($options as $value) {
                 </div>  			
     			<div id="author-email">
                     <a class="email" title="<?php echo antispambot($authordata->user_email); ?>" href="mailto:<?php echo antispambot($authordata->user_email); ?>"><?php _e('Email ', 'thematic') ?><span class="fn n"><span class="given-name"><?php echo $authordata->first_name; ?></span> <span class="family-name"><?php echo $authordata->last_name; ?></span></span></a>
-                    <a class="url"  style="display:none;" href="<?php echo get_option('home') ?>/"><?php thm_bloginfo('name', TRUE) ?></a>   
+                    <a class="url"  style="display:none;" href="<?php thm_bloginfo('home', TRUE) ?>/"><?php thm_bloginfo('name', TRUE) ?></a>   
                 </div>
 			</div><!-- #author-info -->
 <?php } ?>
 			
-<?php thematic_author_loop() ?>
+<?php thematic_authorloop() ?>
 
 			<?php thematic_navigation_below();?>
 	
