b20c2c98ea4569cf4c2f69426a3db8673627bad5
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2010-01-22 15:35:38 +0000

    ADDED: documentation

--- links.php
+++ links.php
@@ -5,38 +5,76 @@ Template Name: Links Page
 ?>
 <?php
 
-get_header();
+    // calling the header.php
+    get_header();
 
-thematic_abovecontainer();
+    // action hook for placing content above #container
+    thematic_abovecontainer();
 
-?>	
+?>
+
 	<div id="container">
 		<div id="content">
 
-<?php the_post() ?>
+            <?php
+            
+            the_post();
+            
+            ?>
+            
 			<div id="post-<?php the_ID(); ?>" class="<?php thematic_post_class() ?>">
-    			<?php thematic_postheader(); ?>
+    			<?php
+                
+                thematic_postheader();
+                
+                ?>
+                
 				<div class="entry-content">
-<?php the_content() ?>
+                
+                    <?php
+                    
+                    the_content();
+                    
+                    ?>
 
 					<ul id="links-page" class="xoxo">
-<?php wp_list_bookmarks(list_bookmarks_args()) ?>
+                    
+                        <?php
+                        
+                        wp_list_bookmarks(list_bookmarks_args());
+                        
+                        ?>
+                        
 					</ul>
-<?php edit_post_link(__('Edit', 'thematic'),'<span class="edit-link">','</span>') ?>
+                    
+                    <?php
+                    
+                    edit_post_link(__('Edit', 'thematic'),'<span class="edit-link">','</span>');
+                    
+                    ?>
 
 				</div>
 			</div><!-- .post -->
 
-<?php if ( get_post_custom_values('comments') ) comments_template() // Add a key/value of "comments" to enable comments on pages! ?>
+            <?php 
+        
+            if ( get_post_custom_values('comments') ) 
+                comments_template() // Add a key/value of "comments" to enable comments on pages!
+        
+            ?>
 
 		</div><!-- #content -->
 	</div><!-- #container -->
 
 <?php 
 
+    // action hook for placing content below #container
     thematic_belowcontainer();
 
+    // calling the standard sidebar 
     thematic_sidebar();
     
+    // calling footer.php
     get_footer();
+
 ?>
