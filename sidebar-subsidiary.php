38d90b4138741727d098f4c11eeaa6c570785555
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2010-01-23 21:02:57 +0000

    ADDED: documentation
    git-svn-id: http://thematic.googlecode.com/svn/trunk@612 25245320-ac48-0410-b4b3-eb13123354aa

--- sidebar-subsidiary.php
+++ sidebar-subsidiary.php
@@ -1,5 +1,12 @@
-<?php thematic_abovesubasides() ?>
+<?php
 
-<?php widget_area_subsidiaries ?>
+    // action hook for placing content above the 'subsidiaries' widget area
+    thematic_abovesubasides();
+
+    // action hook for creating the 'subsidiaries' widget area
+    widget_area_subsidiaries();
     
-<?php thematic_belowsubasides() ?>
+    // action hook for placing content below the 'subsidiaries' widget area
+    thematic_belowsubasides();
+    
+?>
--- tag.php
+++ tag.php
@@ -1,40 +1,59 @@
 <?php
-global $options;
-foreach ($options as $value) {
-    if (get_option( $value['id'] ) === FALSE) { $$value['id'] = $value['std']; }
-    else { $$value['id'] = get_option( $value['id'] ); }
+    // calling the theme options
+    global $options;
+    foreach ($options as $value) {
+        if (get_option( $value['id'] ) === FALSE) { 
+            $$value['id'] = $value['std']; 
+        } else {
+            $$value['id'] = get_option( $value['id'] );
+        }
     }
+
+    // calling the header.php
+    get_header();
+
+    // action hook for placing content above #container
+    thematic_abovecontainer();
+
 ?>
-<?php
 
-get_header();
-
-thematic_abovecontainer();
-
-?>	
 	<div id="container">
 		<div id="content">
 
-	<?php thematic_page_title() ?>
+            <?php
+        
+            // displays the page title
+            thematic_page_title();
 
-			<?php thematic_navigation_above();?>
+            // create the navigation above the content
+            thematic_navigation_above();
 			
-<?php thematic_above_tagloop() ?>			
+            // action hook for placing content above the tag loop
+            thematic_above_tagloop();		
 
-<?php thematic_tagloop() ?>
+            // action hook creating the tag loop
+            thematic_tagloop();
 
-<?php thematic_below_tagloop() ?>			
+            // action hook for placing content below the tag loop
+            thematic_below_tagloop();
 
-			<?php thematic_navigation_below();?>
+            // create the navigation below the content
+            thematic_navigation_below();
+            
+            ?>
 
 		</div><!-- #content -->
 	</div><!-- #container -->
 
 <?php 
 
+    // action hook for placing content below #container
     thematic_belowcontainer();
 
+    // calling the standard sidebar 
     thematic_sidebar();
     
+    // calling footer.php
     get_footer();
+
 ?>
