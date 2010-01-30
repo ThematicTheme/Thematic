2f86d721f168a7d7861c019b46834fba3cb31cea
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2010-01-30 15:03:50 +0000

    REMOVED: call the theme options
    git-svn-id: http://thematic.googlecode.com/svn/trunk@623 25245320-ac48-0410-b4b3-eb13123354aa

--- search.php
+++ search.php
@@ -1,13 +1,4 @@
 <?php
-    // calling the theme options
-    global $options;
-    foreach ($options as $value) {
-        if (get_option( $value['id'] ) === FALSE) { 
-            $$value['id'] = $value['std']; 
-        } else {
-            $$value['id'] = get_option( $value['id'] );
-        }
-    }
 
     // calling the header.php
     get_header();
