0b2d4dd9c88f91ecbcacb110c27f622f780d0522
emhr@submersible.me <emhr@submersible.me@25245320-ac48-0410-b4b3-eb13123354aa>
2010-06-07 10:28:26 +0000

    ADDED: to filter to control the length of thematic_search_form()
    git-svn-id: http://thematic.googlecode.com/svn/trunk@667 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/widgets-extensions.php
+++ library/extensions/widgets-extensions.php
@@ -1,17 +1,18 @@
 <?php
 
 function thematic_search_form() {
+				$search_form_length = apply_filters('thematic_search_form_length', '32');
 				$search_form = "\n" . "\t";
 				$search_form .= '<form id="searchform" method="get" action="' . get_bloginfo('home') .'">';
 				$search_form .= "\n" . "\t" . "\t";
 				$search_form .= '<div>';
 				$search_form .= "\n" . "\t" . "\t". "\t";
 				if (is_search()) {
-						$search_form .= '<input id="s" name="s" type="text" value="' . wp_specialchars(stripslashes($_GET['s']), true) .'" size="32" tabindex="1" />';
+						$search_form .= '<input id="s" name="s" type="text" value="' . wp_specialchars(stripslashes($_GET['s']), true) .'" size="' . $search_form_length . '" tabindex="1" />';
 				} else {
 						$value = __('To search, type and hit enter', 'thematic');
 						$value = apply_filters('search_field_value',$value);
-						$search_form .= '<input id="s" name="s" type="text" value="' . $value . '" onfocus="if (this.value == \'' . $value . '\') {this.value = \'\';}" onblur="if (this.value == \'\') {this.value = \'' . $value . '\';}" size="32" tabindex="1" />';
+						$search_form .= '<input id="s" name="s" type="text" value="' . $value . '" onfocus="if (this.value == \'' . $value . '\') {this.value = \'\';}" onblur="if (this.value == \'\') {this.value = \'' . $value . '\';}" size="'. $search_form_length .'" tabindex="1" />';
 				}
 				$search_form .= "\n" . "\t" . "\t". "\t";
 
