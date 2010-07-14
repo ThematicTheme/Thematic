3a9a59f845943636276741f4bdef219f2c427520
downstairsdev <downstairsdev@25245320-ac48-0410-b4b3-eb13123354aa>
2010-07-14 20:53:13 +0000

    Added page template page-fullwidth.php: http://themeshaper.com/forums/topic/new-page-templates-in-thematic

---
+++ page-fullwidth.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * This Full Width template removes the primary and secondary asides so that content
+ * can be displayed the entire width of the #content area.
+ *
+ */
+
+
+    // calling the header.php
+    get_header();
+
+    // action hook for placing content above #container
+    thematic_abovecontainer();
+
+?>
+
+		<div id="container">
+		
+			<?php thematic_abovecontent(); ?>
+		
+			<div id="content">
+	
+	            <?php
+	        
+	            // calling the widget area 'page-top'
+	            get_sidebar('page-top');
+	
+	            the_post();
+	            
+	            thematic_abovepost();
+	        
+	            ?>
+	            
+				<div id="post-<?php the_ID(); ?>" class="<?php thematic_post_class() ?>">
+	            
+	                <?php 
+	                
+	                // creating the post header
+	                thematic_postheader();
+	                
+	                ?>
+	                
+					<div class="entry-content">
+	
+	                    <?php
+	                    
+	                    the_content();
+	                    
+	                    wp_link_pages("\t\t\t\t\t<div class='page-link'>".__('Pages: ', 'thematic'), "</div>\n", 'number');
+	                    
+	                    edit_post_link(__('Edit', 'thematic'),'<span class="edit-link">','</span>') ?>
+	
+					</div>
+				</div><!-- .post -->
+	
+	        <?php
+	        
+	        thematic_belowpost();
+	        
+	        // calling the comments template
+	        thematic_comments_template();
+	        
+	        // calling the widget area 'page-bottom'
+	        get_sidebar('page-bottom');
+	        
+	        ?>
+	
+			</div><!-- #content -->
+			
+			<?php thematic_belowcontent(); ?> 
+			
+		</div><!-- #container -->
+
+<?php 
+
+    // action hook for placing content below #container
+    thematic_belowcontainer();
+    
+    // calling footer.php
+    get_footer();
+
+?>
