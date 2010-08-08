abef36444ad64277bcad98bdc31ae96d99b3f2f4
emhr@submersible.me <emhr@submersible.me@25245320-ac48-0410-b4b3-eb13123354aa>
2010-08-08 17:00:31 +0000

    Fixed: Conditional display of comment-edit anchor and meta separator.

--- library/extensions/discussion-extensions.php
+++ library/extensions/discussion-extensions.php
@@ -13,14 +13,17 @@ if (function_exists('childtheme_override_commentmeta'))  {
 					sprintf( __('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment">Permalink</a>', 'thematic' ),
 					get_comment_date(),
 					get_comment_time(),
-					'#comment-' . get_comment_ID() ) .
-					' <span class="meta-sep">|</span> ' .
-					sprintf('<span class="edit-link"><a class="comment-edit-link" href="%1$s" title="%2$s">%3$s</a></span>',
-					get_edit_comment_link(),
-					__( 'Edit comment' ),
-					__( 'Edit', 'thematic' ) ) .
-					'</div>' . "\n";
-
+					'#comment-' . get_comment_ID() );
+							
+		if ( get_edit_comment_link() ) {
+			$content .=	sprintf(' <span class="meta-sep">|</span><span class="edit-link"> <a class="comment-edit-link" href="%1$s" title="%2$s">%3$s</a></span>',
+						get_edit_comment_link(),
+						__( 'Edit comment' ),
+						__( 'Edit', 'thematic' ) );
+			}
+		
+		$content .= '</div>' . "\n";
+			
 		return $print ? print(apply_filters('thematic_commentmeta', $content)) : apply_filters('thematic_commentmeta', $content);
 
 	} // end thematic_commentmeta
