f9dc0e1ae3a70b12a5a42d95617e8070f2a4c0c6
emhr@submersible.me <emhr@submersible.me@25245320-ac48-0410-b4b3-eb13123354aa>
2010-09-07 16:40:35 +0000

    FIxes: Notice for undefined constant assumed has_nav_menu
    git-svn-id: http://thematic.googlecode.com/svn/trunk@719 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/header-extensions.php
+++ library/extensions/header-extensions.php
@@ -499,7 +499,7 @@ function thematic_header() {
 	    		
 	    	<?php 
 	    		
-	    	if ((function_exists(has_nav_menu)) && (has_nav_menu(apply_filters('thematic_primary_menu_id', 'primary-menu')))) {
+	    	if ((function_exists("has_nav_menu")) && (has_nav_menu(apply_filters('thematic_primary_menu_id', 'primary-menu')))) {
 	    		echo  wp_nav_menu(thematic_nav_menu_args());
     		} else {
     			echo  thematic_add_menuclass(wp_page_menu(thematic_page_menu_args()));	
