62432a120a92f1652853339b88480bea73282349
chris.gossmann <chris.gossmann@25245320-ac48-0410-b4b3-eb13123354aa>
2010-09-11 12:56:00 +0000

    Added Thematic's browser / OS class to WorpPress' body_class(). Menu will now display properly with the Test Data.
    git-svn-id: http://thematic.googlecode.com/svn/trunk@721 25245320-ac48-0410-b4b3-eb13123354aa

--- library/extensions/dynamic-classes.php
+++ library/extensions/dynamic-classes.php
@@ -330,6 +330,75 @@ if (function_exists('childtheme_override_body_class'))  {
 		return $print ? print($c) : $c;
 	}
 }
+
+// Add browser CSS class to body_class()
+add_filter('body_class','browser_class_names');
+function browser_class_names($classes) {
+	// add 'class-name' to the $classes array
+	$classes[] = 'class-name';
+	$browser = $_SERVER[ 'HTTP_USER_AGENT' ];
+		
+	// Mac, PC ...or Linux
+	if ( preg_match( "/Mac/", $browser ) ){
+		$classes[] = 'mac';
+			
+	} elseif ( preg_match( "/Windows/", $browser ) ){
+		$classes[] = 'windows';
+			
+	} elseif ( preg_match( "/Linux/", $browser ) ) {
+		$classes[] = 'linux';
+	
+	} else {
+		$classes[] = 'unknown-os';
+	}
+		
+	// Checks browsers in this order: Chrome, Safari, Opera, MSIE, FF
+	if ( preg_match( "/Chrome/", $browser ) ) {
+		$classes[] = 'chrome';
+	
+		preg_match( "/Chrome\/(\d.\d)/si", $browser, $matches);
+		$ch_version = 'ch' . str_replace( '.', '-', $matches[1] );      
+		$classes[] = $ch_version;
+	
+	} elseif ( preg_match( "/Safari/", $browser ) ) {
+		$classes[] = 'safari';
+				
+		preg_match( "/Version\/(\d.\d)/si", $browser, $matches);
+		$sf_version = 'sf' . str_replace( '.', '-', $matches[1] );      
+		$classes[] = $sf_version;
+				
+	} elseif ( preg_match( "/Opera/", $browser ) ) {
+		$classes[] = 'opera';
+				
+		preg_match( "/Opera\/(\d.\d)/si", $browser, $matches);
+		$op_version = 'op' . str_replace( '.', '-', $matches[1] );      
+		$classes[] = $op_version;
+				
+	} elseif ( preg_match( "/MSIE/", $browser ) ) {
+		$classes[] = 'msie';
+				
+		if( preg_match( "/MSIE 6.0/", $browser ) ) {
+			$classes[] = 'ie6';
+		} elseif ( preg_match( "/MSIE 7.0/", $browser ) ){
+			$classes[] = 'ie7';
+		} elseif ( preg_match( "/MSIE 8.0/", $browser ) ){
+			$classes[] = 'ie8';
+		}
+				
+	} elseif ( preg_match( "/Firefox/", $browser ) && preg_match( "/Gecko/", $browser ) ) {
+			$classes[] = 'firefox';
+				
+			preg_match( "/Firefox\/(\d)/si", $browser, $matches);
+			$ff_version = 'ff' . str_replace( '.', '-', $matches[1] );      
+			$classes[] = $ff_version;
+				
+	} else {
+		$classes[] = 'unknown-browser';
+	}
+	// return the $classes array
+	return $classes;
+}
+
 	
 if (function_exists('childtheme_override_post_class'))  {
 	function thematic_post_class() {
