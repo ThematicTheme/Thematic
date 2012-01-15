570e67df26390e6301ad9811d2dd646a71bef690
eugene.marseilles.hornot.robinson@gmail.com <eugene.marseilles.hornot.robinson@gmail.com@25245320-ac48-0410-b4b3-eb13123354aa>
2012-01-15 20:59:05 +0000

    Added docblock. Rearranged file contents. Removed empty spaces and end of file tag.
    git-svn-id: http://thematic.googlecode.com/svn/trunk@779 25245320-ac48-0410-b4b3-eb13123354aa

--- thematicsamplechildtheme/functions.php
+++ thematicsamplechildtheme/functions.php
@@ -1,25 +1,20 @@
 <?php
+/**
+ * Custom Child Theme Functions
+ *
+ * This file's parent directory can be moved to the wp-content/themes directory 
+ * to allow this Child theme to be activated in the Appearance - Themes section of the WP-Admin.
+ *
+ * Included are a set of constants that can be defined to customize aspects of Thematic's 
+ * functionality, as well as a sample function that will add a home link to your menu.
+ * "Uncomment" or add more to cusomize the functionality of your Child Theme.
+ *
+ * More ideas can be found on "A Guide To Customizing The Thematic Theme Framework" 
+ * @link http://themeshaper.com/thematic-for-wordpress/guide-customizing-thematic-theme-framework/
+ *
+ * @package SampleChildTheme
+ */
 
-//
-//  Custom Child Theme Functions
-//
-
-// I've included a "commented out" sample function below that'll add a home link to your menu
-// More ideas can be found on "A Guide To Customizing The Thematic Theme Framework" 
-// http://themeshaper.com/thematic-for-wordpress/guide-customizing-thematic-theme-framework/
-
-// Adds a home link to your menu
-// http://codex.wordpress.org/Template_Tags/wp_page_menu
-//function childtheme_menu_args($args) {
-//    $args = array(
-//        'show_home' => 'Home',
-//        'sort_column' => 'menu_order',
-//        'menu_class' => 'menu',
-//        'echo' => false
-//    );
-//	return $args;
-//}
-//add_filter('wp_page_menu_args','childtheme_menu_args');
 
 // Unleash the power of Thematic's dynamic classes
 // 
@@ -34,4 +29,16 @@
 //
 // define('THEMATIC_COMPATIBLE_FEEDLINKS', true);
 
-?>
+
+// Adds a home link to your menu
+// http://codex.wordpress.org/Template_Tags/wp_page_menu
+//function childtheme_menu_args($args) {
+//    $args = array(
+//        'show_home' => 'Home',
+//        'sort_column' => 'menu_order',
+//        'menu_class' => 'menu',
+//        'echo' => false
+//    );
+//	return $args;
+//}
+//add_filter('wp_page_menu_args','childtheme_menu_args');
