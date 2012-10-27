6cf50399adb5ab35a5ea34db0788ef41259bba27
emhr <gene@thematictheme.com>
2012-10-27 15:55:29 -0400

    Sample childtheme edits
    Revised styles and inline comments

--- thematicsamplechildtheme/functions.php
+++ thematicsamplechildtheme/functions.php
@@ -21,12 +21,17 @@
  * Define theme setup
  */
 function childtheme_setup() {
-	// For backwards compatibility with some older child themes
-	// add_theme_support( 'thematic_legacy_feedlinks' ); // replaces define( 'THEMATIC_COMPATIBLE_FEEDLINKS', true );
-	// add_theme_support( 'thematic_legacy_body_class' ); // replaces define( 'THEMATIC_COMPATIBLE_BODY_CLASS', true );
-	// add_theme_support( 'thematic_legacy_post_class' ); // replaces define( 'THEMATIC_COMPATIBLE_POST_CLASS', true );
-	// add_theme_support( 'thematic_legacy_comment_form' ); // replaces define( 'THEMATIC_COMPATIBLE_COMMENT_FORM', true );
-	// add_theme_support( 'thematic_legacy_comment_handling' ); // replaces define( 'THEMATIC_COMPATIBLE_COMMENT_HANDLING', true );
+	
+	/* The Following add_theme_support functions 
+	 * will enable legacy Thematic Features
+	 * if uncommented.
+	 */
+	 
+	// add_theme_support( 'thematic_legacy_feedlinks' );
+	// add_theme_support( 'thematic_legacy_body_class' );
+	// add_theme_support( 'thematic_legacy_post_class' );
+	// add_theme_support( 'thematic_legacy_comment_form' );
+	// add_theme_support( 'thematic_legacy_comment_handling' );
 	
 	
 	/*
@@ -50,26 +55,26 @@ function childtheme_setup() {
 	 */
 	add_theme_support( 'custom-header', array(
 		// Header image default
-		'default-image'	=> '',
+		'default-image' => '',
 		// Header text display default
-		'header-text'	=> true,
+		'header-text' => true,
 		// Header text color default
-		'default-text-color'	=> '000',
+		'default-text-color' => '000',
 		// Header image width (in pixels)
 		'width'	=> '940',
 		// Header image height (in pixels)
-		'height'	=> '235',
+		'height' => '235',
 		// Header image random rotation default
-		'random-default'	=> false,
+		'random-default' => false,
 		// Template header style callback
-		'wp-head-callback'	=> 'childtheme_header_style',
+		'wp-head-callback' => 'childtheme_header_style',
 		// Admin header style callback
-		'admin-head-callback'	=> 'childtheme_admin_header_style'
-			) 
+		'admin-head-callback' => 'childtheme_admin_header_style'
+		) 
 	);
 	
 }
-add_action('after_setup_theme', 'childtheme_setup', 20);
+add_action('thematic_child_init', 'childtheme_setup');
 
 
 /**
@@ -77,6 +82,10 @@ add_action('after_setup_theme', 'childtheme_setup', 20);
  *
  * Defines the front-end style definitions for 
  * the custom image header.
+ * This style declaration will be output in the <head> of the
+ * document just before the closing </head> tag.
+ * Inline Syles and !important declarations 
+ * can be used to override these styles.
  *
  * @link http://codex.wordpress.org/Function_Reference/get_header_image get_header_image()
  * @link http://codex.wordpress.org/Function_Reference/get_header_textcolor get_header_textcolor()
@@ -85,23 +94,36 @@ function childtheme_header_style() {
 	?>	
 	<style type="text/css">
 	<?php
-	/* Sets header image as background for div#branding */
+	/* Declares the header image from the settings
+	 * saved in WP-Admin > Appearance > Header
+	 * as the background-image for div#branding.
+	 */
 	if ( get_header_image() && HEADER_IMAGE != get_header_image() ) {
 		?>
 		#branding {
-			background:url('<?php header_image(); ?>') no-repeat center top;
-			overflow: hidden;
+			background:url('<?php header_image(); ?>') no-repeat 0 100%;
+			margin-bottom:28px;
+    		padding:44px 0 <?php echo HEADER_IMAGE_HEIGHT; ?>px 0; /* Bottom padding is the same height as the image */
+    		overflow: visible;
+}
 		}
 		<?php if ( 'blank' != get_header_textcolor() ) { ?>
 		#blog-title, #blog-title a {
 			color:#000;
 		}
+		#blog-description {	
+			padding-bottom: 22px;
+		}
 		<?php
-		}	
+		}
+		
 	}
 	?>
 	<?php
-	/* Sets text color for #blog-title and #blog-description */
+	/* This delcares text color for the Blog title and Description
+	 * from the settings saved in WP-Admin > Appearance > Header\
+	 * If not set the deafault color is set to #000 
+	 */
 	if ( get_header_textcolor() ) {
 		?>
 		#blog-title, #blog-title a, #blog-description {
@@ -109,13 +131,25 @@ function childtheme_header_style() {
 		}
 		<?php
 	}
-	/* Removes header text if desired */
+	/* Removes header text if the
+	 * "Do not diplay header text…" setting is saved
+	 * in WP-Admin > Appearance > Header
+	 */
 	if ( ! display_header_text() ) {
 		?>
+		#branding {
+			background-position: center bottom;
+			background-repeat: no-repeat;
+			margin-top: 32px;
+		}
 		#blog-title, #blog-title a, #blog-description {
 			display:none;
 		}
-		#branding { height: 235px; padding:0;}
+		#branding { 
+			height:<?php echo HEADER_IMAGE_HEIGHT; ?>px; 
+			width:940px;
+			padding:0; 
+		}
 		<?php
 	}
 	?>
@@ -127,37 +161,41 @@ function childtheme_header_style() {
 /**
  * Custom Image Header Admin Callback
  *
- * Callback to define the admin (back-end) style
- * definitions for the custom image header. Customize
- * css to match your theme defaults.
+ * Callback to defines the admin (back-end) style
+ * definitions for the custom image header.
+ * Customize the css to match your theme defaults.
+ * The !important declarations override inline admin styles
+ * to better represent a WYSIWYG of the front-end styling
+ * that this child theme is currently designed to display.
  */
 function childtheme_admin_header_style() {
 	?>
 	<style type="text/css">
 	#headimg {
-		width: <?php echo HEADER_IMAGE_WIDTH; ?>px;
-		height: <?php echo HEADER_IMAGE_HEIGHT; ?>px;
+		background-position: left bottom; 
+		background-repeat:no-repeat;
+		border:0 !important;   
+		height:auto !important;
+		padding:0 0 <?php echo HEADER_IMAGE_HEIGHT + 22; /* change the added integer (22) to match your desired top padding */?>px 0;
+		margin:0 0 28px 0;
 	}
-	#headimg h1,
-	#headimg div {
-	border:0;
-	}
+	
 	#headimg h1 {
 	    font-family:Arial,sans-serif;
 	    font-size:34px;
 	    font-weight:bold;
 	    line-height:40px;
-		padding-top:88px;
 		margin:0;
 	}
-	#headimg h1 a {
+	#headimg a {
+		color: #000;
 		text-decoration: none;
 	}
-	#headimg div {
-	    font-size:13px;
-	    font-style:italic;
-	}
+	#desc{
+		font-family: Georgia;
+    	font-size: 13px;
+    	font-style: italic;
+    }
 	</style>
 	<?php
 }
-
