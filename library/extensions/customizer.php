6e3d138516bc3c7bc631f3892aba089125b9bede
Karin Taliga <invistruct@gmail.com>
2013-11-19 11:19:29 +0100

    Register Theme Customizer settings and sections

--- library/extensions/customizer.php
+++ library/extensions/customizer.php
@@ -7,3 +7,44 @@
  */
 
 
+/**
+ * Implement Theme Customizer additions and adjustments.
+ *
+ * @since 2.0
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function thematic_customize_register( $wp_customize ) {
+	
+	// Add postMessage support for site title and description.
+	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+	
+	// Add section for thematic footer options 
+    $wp_customize->add_section( 'thematic_footer_text', array(
+        'title'			=> __( 'Footer', 'tornfeldt'),
+        'priority'		=> 135,
+    ) );
+	
+	// Get the default thematic footer text 
+	$thematic_defaults = thematic_default_opt();
+	$thematic_default_footertext = $thematic_defaults['footer_txt'];
+
+	// Add setting for footer text 
+    $wp_customize->add_setting( 'thematic_theme_opt[footer_txt]', array(
+        'default'		=> $thematic_default_footertext,
+		'type'			=> 'option',
+		'capability'	=> 'edit_theme_options',
+		'transport'		=> 'postMessage'
+    ) );
+ 
+	// Add control for footer text 
+    $wp_customize->add_control( 'thematic_theme_opt[footer_txt]', array(
+        'label'			=> __('Footer text', 'thematic'),
+        'section'		=> 'thematic_footer_text',
+        'type'			=> 'text',
+		'settings'		=> 'thematic_theme_opt[footer_txt]'
+    ) );
+	
+}
+add_action( 'customize_register', 'thematic_customize_register' );
