af394947f37eb656fbb198b1fcd9404797415d68
Karin Taliga <invistruct@gmail.com>
2014-01-16 11:37:55 +0100

    Conditionally activate superfish in thematic.js

--- library/js/thematic.js
+++ library/js/thematic.js
@@ -22,4 +22,40 @@
 			nav.toggleClass( 'toggled-on' );
 		} );
 	} )();
+	
+	/**
+	 * Enable superfish on larger screens, but only if it exists
+	 * 
+	 * See @link{http://stackoverflow.com/questions/6748301/disable-superfish-on-resize-event}
+	 */
+	( function() {
+		var body = $( 'body' );
+		var breakpoint = 600;
+		var sf = $( 'ul.sf-menu' );
+		
+		// only activate superfish if scripts are loaded
+		if( typeof( sf.superfish ) === 'function' ) {
+			
+			if( body.width() >= breakpoint ) {
+				// enable superfish when the page first loads if we're on desktop
+				sf.superfish({ 
+					delay:       400,                               // delay on mouseout 
+					animation:   {opacity:'show',height:'show'},    // fade-in and slide-down animation 
+					speed:       'fast',                            // faster animation speed 
+					autoArrows:  false,                             // disable generation of arrow mark-up 
+					dropShadows: false                              // disable drop shadows 
+				});
+			}
+		
+			$( window ).resize( function() {
+				if( body.width() >= breakpoint && !sf.hasClass('sf-js-enabled') ) {
+					// you only want SuperFish to be re-enabled once (sf.hasClass)
+					sf.superfish( 'init' );
+				} else if( body.width() < breakpoint ) {
+					// smaller screen, disable SuperFish
+					sf.superfish( 'destroy' );
+				}
+			});
+		}
+	} )();
 } )( jQuery );
