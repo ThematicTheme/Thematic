3b813d0bf3a6b61d71169cdb1944425484b7929d
Karin Taliga <invistruct@gmail.com>
2014-03-20 11:33:47 +0100

    Add more unit tests for layout filters
    Add test for trying to switch to nonexistent layout
    Add test for changing the default layout
    Add test for switching to nonexisting layout when default
      layout has been changed

--- library/tests/test-dynamic-classes.php
+++ library/tests/test-dynamic-classes.php
@@ -10,10 +10,18 @@
  */
 class TestDynamicClasses extends Thematic_UnitTestCase {
 
+	function tearDown() {
+		remove_filter( 'thematic_default_theme_layout', array( $this, 'switch_layout' ) );
+
+		parent::tearDown();
+	}
+
+
 	function test_thematic_body_class_layout() {
 		$body_classes = thematic_body_class( array() );
 		$this->assertContains( 'right-sidebar', $body_classes );	
 	}
+
 	
 	function test_thematic_filter_current_theme_layout() {
 		add_filter( 'thematic_current_theme_layout', array( $this, 'switch_layout' ) );
@@ -24,6 +32,7 @@ class TestDynamicClasses extends Thematic_UnitTestCase {
 		$this->assertContains( 'left-sidebar', $body_classes );	
 	}
 
+
 	function test_thematic_add_available_layout() {
 		add_filter( 'thematic_available_theme_layouts', array( $this, 'add_layout' ) );
 
@@ -31,6 +40,7 @@ class TestDynamicClasses extends Thematic_UnitTestCase {
 
 		$this->assertArrayHasKey( 'half-sidebar', $available_layouts );
 	}
+
 
 	function test_thematic_remove_available_layout() {
 		add_filter( 'thematic_available_theme_layouts', array( $this, 'remove_layout' ) );
@@ -40,9 +50,41 @@ class TestDynamicClasses extends Thematic_UnitTestCase {
 		$this->assertArrayNotHasKey( 'right-sidebar', $available_layouts );
 	}
 
+
+	function test_thematic_unavailable_layout() {
+		add_filter( 'thematic_current_theme_layout', array( $this, 'unavailable_layout' ) );
+
+		$body_classes = thematic_body_class( array() );
+		$this->assertNotContains( 'wacko', $body_classes );
+		$this->assertContains( 'right-sidebar', $body_classes );
+	}
+
+
+	function test_thematic_change_default_layout() {
+		add_filter( 'thematic_default_theme_layout', array( $this, 'switch_layout' ) );
+
+		$this->theme_options['layout'] = apply_filters( 'thematic_default_theme_layout', $this->theme_options['layout'] );
+
+		$this->assertEquals( $this->theme_options['layout'], 'left-sidebar' );
+	}
+
+
+	function test_thematic_unavailable_layout_different_default() {
+		add_filter( 'thematic_current_theme_layout', array( $this, 'unavailable_layout' ) );
+
+		add_filter( 'thematic_default_theme_layout', array( $this, 'switch_layout' ) );
+		$this->theme_options['layout'] = apply_filters( 'thematic_default_theme_layout', $this->theme_options['layout'] );
+
+		$body_classes = thematic_body_class( array() );
+		$this->assertNotContains( 'wacko', $body_classes );
+		$this->assertContains( 'left-sidebar', $body_classes );
+	}
+
+
 	function switch_layout( $layout ) {
 		return 'left-sidebar';
 	}
+
 
 	function add_layout( $layouts ) {
 		$layouts['half-sidebar'] = array(
@@ -52,9 +94,15 @@ class TestDynamicClasses extends Thematic_UnitTestCase {
 		return $layouts;
 	}
 
+
 	function remove_layout( $layouts ) {
 		unset( $layouts['right-sidebar'] );
 		return $layouts;
+	}
+
+
+	function unavailable_layout() {
+		return 'wacko';
 	}
 
 }
